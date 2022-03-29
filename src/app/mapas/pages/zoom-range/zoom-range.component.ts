40b31b87d87161367fc035e515e03894e361f122
Dylan <dylan204.play@gmail.com>
2022-03-29 16:39:10 -0500

    avance2

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -20,26 +26,47 @@ import * as mapboxgl from 'mapbox-gl';
 
         position: fixed;
         z-index: 999;
+        width: 400px;
       }
     `,
   ],
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-75.56988526611688, 6.312262640606767];
   constructor() {}
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-75.56988526611688, 6.312262640606767],
+      center: this.center,
       zoom: this.zoomLevel,
     });
 
     this.mapa.on('zoom', () => {
       this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', () => {
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    //Movimiento del mapa
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
     });
   }
 
@@ -49,5 +76,9 @@ export class ZoomRangeComponent implements AfterViewInit {
 
   zoomIn() {
     this.mapa.zoomIn();
+  }
+
+  zoomCambio(valor: any) {
+    this.mapa.zoomTo(Number(valor));
   }
 }
